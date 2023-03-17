3ca79d959d8d3f9265b3c763f0ff769680e928fe
huanglin <huanglin@feysh.com>
2023-03-17 21:32:20 +0800

    Change FileUtils API

--- FileUtils.js
+++ FileUtils.js
@@ -2,13 +2,27 @@ import fs from 'fs';
 import { levenshteinDistance } from './Utils.js';
 
 class File {
-    constructor(path) {
+    constructor(path, root='') {
         this.path = path;
+        this.root = root
         this.fileName = path.split('/').splice(-1)[0];
+        this.formatedFileName = File.formatFileName(this.fileName);
     }
 
     toString() {
-        return this.fileName;
+        return this.formatedFileName;
+    }
+    
+    static filterFileName(fileName) {
+        return fileName.endsWith('.cs') || fileName.endsWith('.java');
+    }
+
+    static formatFileName(fileName) {
+        return fileName.replace(new RegExp(`\.(cs)?(java)?$`), '');
+    }
+
+    getText() {
+        return fs.readFileSync(this.root + this.path, 'utf-8');
     }
 }
 
@@ -17,29 +31,50 @@ class Folder {
 }
 
 
-function ls(folder) {
-    if (typeof folder === 'string') folder = getFileTree(folder);
-    return Object.keys(folder);
+const fileTreeBuffer = {};
+
+function visit(root, path = '') {              // 结果仅保留（去后缀名的）代码文件
+    if (fileTreeBuffer[root] != undefined)
+        return fileTreeBuffer[root];
+    try {
+        const dir = fs.readdirSync(root + path);
+        const folder = new Folder();
+        dir.forEach(element => {
+            const child = visit(root, path + element + '/');
+            if (child !== null)
+                folder[element] = child;
+        });
+        fileTreeBuffer[root + path] = folder;
+        return folder;
+    } catch (e) {
+        if (e.code === 'ENOTDIR') {                 // 'src' is a file rather than dir
+            path = path.substring(0, path.length - 1)  // remove extra '/'
+            if (!File.filterFileName(path))
+                return null;
+            return new File(path, root);
+        }
+        throw e;
+    }
 }
 
 
 function getFolders(folder) {
-    if (typeof folder === 'string') folder = getFileTree(folder);
+    if (typeof folder === 'string') folder = visit(folder);
     return Object.values(folder).filter(e => e instanceof Folder);
 }
 
 
 function getFiles(folder) {
-    if (typeof folder === 'string') folder = getFileTree(folder);
+    if (typeof folder === 'string') folder = visit(folder);
     return Object.values(folder).filter(e => e instanceof File);
 }
 
 
 function getAllFiles(folder) {                      // result is sorted by file name
-    if (typeof folder === 'string') folder = getFileTree(folder);
+    if (typeof folder === 'string') folder = visit(folder);
     let result = [];
     result.push(...getFiles(folder));
-    result.sort((file1, file2) => file1.fileName < file2.fileName ? -1 : 1);
+    result.sort((file1, file2) => file1.toString() < file2.toString() ? -1 : 1);
     getFolders(folder).forEach(f => result = merge(result, getAllFiles(f)));
     return result;
 
@@ -52,7 +87,7 @@ function getAllFiles(folder) {                      // result is sorted by file 
             } else if (bi >= b.length) {
                 result[i] = a[ai++];
             } else {
-                if (a[ai].fileName < b[bi].fileName) {
+                if (a[ai].toString() < b[bi].toString()) {
                     result[i] = a[ai++];
                 } else {
                     result[i] = b[bi++];
@@ -64,98 +99,64 @@ function getAllFiles(folder) {                      // result is sorted by file 
 }
 
 
-function filterFileName(fileName) {
-    return fileName.endsWith('.cs') || fileName.endsWith('.java');
-}
-
-function formatFileName(fileName) {
-    return fileName.replace(new RegExp(`\.(cs)?(java)?$`), '');
-}
-
-
-const fileTreeBuffer = {};
-function getFileTree(root, src = '') {              // 结果仅保留（去后缀名的）代码文件
-    if (fileTreeBuffer[root] != undefined)
-        return fileTreeBuffer[root];
-    try {
-        const dir = fs.readdirSync(root + src);
-        const folder = new Folder();
-        dir.forEach(element => {
-            const child = getFileTree(root, src + element + '/');
-            if (child !== null)
-                folder[element] = child;
-        });
-        fileTreeBuffer[root + src] = folder;
-        return folder;
-
-    } catch (e) {
-        if (e.code === 'ENOTDIR') {                 // 'src' is a file rather than dir
-            src = src.substring(0, src.length - 1)  // remove extra '/'
-            if (!filterFileName(src))
-                return null;
-            return new File(formatFileName(src));
-        }
-
-        throw e;
-    }
-}
-
-
 function searchInFolderFuzzy(target, folder, MAX_TOLERANCE = 5) {
-    if (typeof folder === 'string') folder = getFileTree(folder);
-    let targetFileName = target instanceof File ? target.fileName : target.toString();
+    if (typeof folder === 'string') folder = visit(folder);
+    let targetFileName = target.toString();         // target can be a File or just string
     let minDist = targetFileName.length;
     let result = [];
-    dfsIter(target, folder);
+    dfsIter(folder);
     if (minDist > MAX_TOLERANCE) return [];
     return result;
 
-    function dfsIter(target, node) {
-        if (node instanceof File) {
-            const dist = levenshteinDistance(node.fileName, targetFileName);
-            if (dist == minDist) {
-                result.push(node);
-            } else if (dist < minDist) {
-                minDist = dist;
-                result.length = 0;
-                result.push(node);
-            }
-            return;
+    function dfsIter(folder) {
+        for (let file of getFiles(folder)) {
+            handleFile(file);
         }
+        for (let subfolder of getFolders(folder)) {
+            dfsIter(subfolder);
+        }
+    }
 
-        const keys = ls(node);
-        for (let i = 0; i < keys.length; i++) {
-            dfsIter(target, node[keys[i]]);
+    function handleFile(file) {
+        const dist = levenshteinDistance(file.toString(), targetFileName);
+        if (dist == minDist) {
+            result.push(file);
+        } else if (dist < minDist) {
+            minDist = dist;
+            result.length = 0;
+            result.push(file);
         }
     }
 }
 
+
 function searchInFolder(target, folder) {
-    if (typeof folder === 'string') folder = getFileTree(folder);
+    if (typeof folder === 'string') folder = visit(folder);
     const files = getAllFiles(folder);
     return searchInFiles(target, files);
 }
 
+
 function searchInFiles(target, files, left = 0, right = files.length) {      // return all possible result, sort by levenshtein distance of file path
-    let targetFileName = target.toString();
+    let targetFileName = target.toString();         // target can be a File or just string
     if (right - left < 10) {
-        const result = files.filter(e => e.fileName == targetFileName);
+        const result = files.filter(e => e.toString() == targetFileName);
         if (target instanceof File) {
             result.sort((f1, f2) => levenshteinDistance(f1.path, target.path) - levenshteinDistance(f2.path, target.path));
         }
         return result;
     }
     let half = Math.floor((left + right) / 2);
-    if (files[half].fileName == targetFileName) {
+    if (files[half].toString() == targetFileName) {
         let left = half, right = half;
-        while (left >= 0 && files[left].fileName == targetFileName) left--;
-        while (right < files.length && files[right].fileName == targetFileName) right++;
+        while (left >= 0 && files[left].toString() == targetFileName) left--;
+        while (right < files.length && files[right].toString() == targetFileName) right++;
         const result = files.splice(left + 1, right - left - 1);
         if (target instanceof File) {
             result.sort((f1, f2) => levenshteinDistance(f1.path, target.path) - levenshteinDistance(f2.path, target.path));
         }
         return result;
-    } else if (files[half].fileName < targetFileName) {
+    } else if (files[half].toString() < targetFileName) {
         return searchInFiles(target, files, half + 1, right);
     } else {
         return searchInFiles(target, files, left, half);
@@ -163,5 +164,4 @@ function searchInFiles(target, files, left = 0, right = files.length) {      // 
 }
 
 
-
-export { File, Folder, ls, getFiles, getAllFiles, getFolders, getFileTree, searchInFolderFuzzy, searchInFolder, searchInFiles };
+export { File, Folder, getFiles, getFolders, visit as visit, getAllFiles, searchInFolderFuzzy, searchInFolder, searchInFiles };
--- index.js
+++ index.js
@@ -1,40 +1,9 @@
-import { File, ls, getAllFiles, getFileTree, searchInFolderFuzzy, searchInFiles, searchInFolder } from './FileUtils.js';
+import { File, getAllFiles, searchInFolderFuzzy, searchInFolder } from './FileUtils.js';
 import { levenshteinDistance } from './Utils.js';
 
 
 
 
 const src1 = 'src/lucene-main/', src2 = 'src/lucenenet-master/';
-console.log(searchInFolderFuzzy('module-info', src1, ).length);
-console.log(searchInFolderFuzzy('module-in', src1, ).length);
-console.log(searchInFolder('module-info', src1, ).length);
-console.log(searchInFolder('module-in', src1, ).length);
-
-
-// const testData = [
-//     'lucene/core/src/java/org/apache/lucene/analysis/standard/package-info.java',
-//     'lucene/core/src/java/org/apache/lucene/analysis/standard/StandardAnalyzer.java',
-//     'lucene/core/src/java/org/apache/lucene/analysis/standard/StandardTokenizer.java',
-//     'lucene/core/src/java/org/apache/lucene/analysis/standard/StandardTokenizerFactory.java',
-//     'lucene/core/src/java/org/apache/lucene/analysis/standard/StandardTokenizerImpl.java',
-// ];
-
-// function dfsIter(node) {
-//     if (testData.length > 50) return;
-//     if (node instanceof File) {
-//         testData.push(node.path);
-//     } else {
-//         Object.values(node).forEach(child => dfsIter(child));
-//     }
-// }
-// dfsIter(fileTree1);
-
-// testData.forEach(
-//     path => {
-//         console.log(
-//             path,
-//             '\t\t\t\|\t',
-//             searchFileTree(path, fileTree2)?.path
-//         )
-//     }
-// );
+console.log(searchInFolderFuzzy('analyzer', src1, )[0]);
+console.log(searchInFolderFuzzy('analyzer', src1, )[0].getText().length);
